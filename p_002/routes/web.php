d488bac88d8bbbabd40e5ee6f6d7f7bb3008725f
keyfarel <carinsmddd@gmail.com>
2025-02-17 14:51:40 +0700

    chore(jobsheet1): complete p_002 praktikum-2

--- p_002/routes/web.php
+++ p_002/routes/web.php
@@ -1,18 +1,25 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+Route::get('/hello', [WelcomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/user/{name?}', [AboutController::class, 'index']);
+Route::get('/articles/{id}', [ArticleController::class, 'index']);
 
-Route::get('/world', function () {
-    return 'World';
-});
+Route::resource('photos', PhotoController::class);
 
-Route::get('/user/{name?}', function ($name = 'John') {
-    return 'Nama saya ' . $name;
-});
+Route::resource('photos', PhotoController::class)->only([
+    'index', 'show'
+]);
+Route::resource('photos', PhotoController::class)->except([
+    'create', 'store', 'update', 'destroy'
+]);
 
 Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
