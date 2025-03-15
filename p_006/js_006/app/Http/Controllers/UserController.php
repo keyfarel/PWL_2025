0bb90c2ce993ee5abc82d02cec8ab5f9ef33db2e
keyfarel <carinsmddd@gmail.com>
2025-03-16 00:08:35 +0700

    feat(jobsheet-6): implement AJAX for improved user interaction in Routes, UserController, and User Show View
    - Updated routes to support AJAX requests.
    - Refactored UserController to handle AJAX calls and return JSON responses.
    - Modified User Show View to use AJAX for dynamic data fetching and rendering.

--- p_006/js_006/app/Http/Controllers/UserController.php
+++ p_006/js_006/app/Http/Controllers/UserController.php
@@ -34,35 +34,6 @@ class UserController extends Controller
             'activeMenu' => $activeMenu]);
     }
 
-//
-//    list tanpa ajax
-//
-//    public function list(Request $request)
-//    {
-//        $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
-//            ->with('level');
-//
-//        if ($request->level_id) {
-//            $users->where('level_id', $request->level_id);
-//        }
-//
-//        return DataTables::of($users)
-//            ->addIndexColumn()
-//            ->addColumn('aksi', function ($user) {
-//                $btn = '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> ';
-//                $btn .= '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> ';
-//                $btn .= '<form class="d-inline-block" method="POST" action="' . url('/user/' . $user->user_id) . '" style="display:inline;">'
-//                    . csrf_field() . method_field('DELETE') .
-//                    '<button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\'Apakah Anda yakin menghapus data ini?\');">Hapus</button>
-//                </form>';
-//                return $btn;
-//            })
-//            ->rawColumns(['aksi'])
-//            ->make(true);
-//    }
-
-// list dengan ajax
-
     public function list(Request $request)
     {
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
@@ -75,63 +46,16 @@ class UserController extends Controller
         return DataTables::of($users)
             ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex)
             ->addColumn('aksi', function ($user) { // menambahkan kolom aksi
-                $btn = '<button onclick="modalAction(\'' . url('/user/' . $user->user_id .
+                $btn = '<button onclick="modalAction(\'' . url('/user/' . $user->user_id . '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
 
-                        '/show_ajax') . '\')" class="btn btn-info btn-sm">Detail</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/user/' . $user->user_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
 
-                $btn .= '<button onclick="modalAction(\'' . url('/user/' . $user->user_id .
-
-                        '/edit_ajax') . '\')" class="btn btn-warning btn-sm">Edit</button> ';
-
-                $btn .= '<button onclick="modalAction(\'' . url('/user/' . $user->user_id .
-
-                        '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
+                $btn .= '<button onclick="modalAction(\'' . url('/user/' . $user->user_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm">Hapus</button> ';
 
                 return $btn;
             })
             ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html
             ->make(true);
-    }
-
-    public function create()
-    {
-        $breadcrumb = (object)[
-            'title' => 'Tambah User',
-            'list' => ['Home', 'User', 'Tambah']
-        ];
-
-        $page = (object)[
-            'title' => 'Tambah user baru'
-        ];
-
-        $level = LevelModel::all();
-        $activeMenu = 'user';
-
-        return view('user.create', [
-            'breadcrumbs' => $breadcrumb,
-            'page' => $page,
-            'level' => $level,
-            'activeMenu' => $activeMenu
-        ]);
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'username' => 'required|string|min:3|unique:m_user,username', // Unik di tabel m_user, kolom username
-            'nama' => 'required|string|max:100', // Nama harus diisi, berupa string, maksimal 100 karakter
-            'password' => 'required|min:5', // Password harus diisi dan minimal 5 karakter
-            'level_id' => 'required|integer' // Level ID harus diisi dan berupa angka
-        ]);
-
-        UserModel::create([
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => bcrypt($request->password), // Enkripsi password sebelum disimpan
-            'level_id' => $request->level_id
-        ]);
-
-        return redirect('/user')->with('success', 'Data user berhasil disimpan');
     }
 
     public function create_ajax()
@@ -171,53 +95,11 @@ class UserController extends Controller
         return redirect('/');
     }
 
-
-    public function show(string $id)
+    public function show_ajax(string $id)
     {
         $user = UserModel::with('level')->find($id);
 
-        $breadcrumb = (object)[
-            'title' => 'Detail User',
-            'list' => ['Home', 'User', 'Detail']
-        ];
-
-        $page = (object)[
-            'title' => 'Detail user'
-        ];
-
-        $activeMenu = 'user'; // Set menu yang sedang aktif
-
-        return view('user.show', [
-            'breadcrumbs' => $breadcrumb,
-            'page' => $page,
-            'user' => $user,
-            'activeMenu' => $activeMenu
-        ]);
-    }
-
-    public function edit(string $id)
-    {
-        $user = UserModel::find($id);
-        $level = LevelModel::all();
-
-        $breadcrumb = (object)[
-            'title' => 'Edit User',
-            'list' => ['Home', 'User', 'Edit']
-        ];
-
-        $page = (object)[
-            'title' => 'Edit user'
-        ];
-
-        $activeMenu = 'user'; // set menu yang sedang aktif
-
-        return view('user.edit', [
-            'breadcrumbs' => $breadcrumb,
-            'page' => $page,
-            'user' => $user,
-            'level' => $level,
-            'activeMenu' => $activeMenu
-        ]);
+        return view('user.show_ajax', compact('user'));
     }
 
     public function edit_ajax(string $id)
@@ -229,27 +111,6 @@ class UserController extends Controller
             'user' => $user,
             'level' => $level
         ]);
-    }
-
-// Menyimpan perubahan data user
-    public function update(Request $request, string $id)
-    {
-        $request->validate([
-            'username' => 'required|string|min:3|unique:m_user,username,' . $id . ',user_id',
-            'nama' => 'required|string|max:100',
-            'password' => 'nullable|min:5',
-            'level_id' => 'required|integer',
-        ]);
-
-        $user = UserModel::find($id);
-        $user->update([
-            'username' => $request->username,
-            'nama' => $request->nama,
-            'password' => $request->password ? bcrypt($request->password) : $user->password,
-            'level_id' => $request->level_id,
-        ]);
-
-        return redirect('/user')->with('success', 'Data user berhasil diubah');
     }
 
     public function update_ajax(Request $request, $id)
@@ -318,22 +179,5 @@ class UserController extends Controller
             }
         }
         return redirect('/');
-    }
-
-    public function destroy(string $id)
-    {
-        $check = UserModel::find($id);
-
-        if (!$check) {
-            return redirect('/user')->with('error', 'Data user tidak ditemukan');
-        }
-
-        try {
-            UserModel::destroy($id);
-
-            return redirect('/user')->with('success', 'Data user berhasil dihapus');
-        } catch (\Illuminate\Database\QueryException $e) {
-            return redirect('/user')->with('error', 'Data user gagal dihapus karena masih terdapat tabel lain yang terkait dengan data ini');
-        }
     }
 }
