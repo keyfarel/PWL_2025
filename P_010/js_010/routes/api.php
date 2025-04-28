a331a43ad827faddb9224588278d99f44eecf0d5
keyfarel <carinsmddd@gmail.com>
2025-04-28 10:50:22 +0700

    chore(jobsheet-10): complete p_010 praktikum-2

--- P_010/js_010/routes/api.php
+++ P_010/js_010/routes/api.php
@@ -3,8 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LoginController;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //    return $request->user();
